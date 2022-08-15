42c5b91565911454f3d646db4d0e19db20523f86
Thedevelop3r <bilalarain444@gmai.com>
2022-08-15 17:07:36 +0500

    progess

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+const connectDB = require("./db/connect");
 const app = express();
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -22,6 +23,9 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI).then(
+      console.log("Mogoose Connection Successfull..")
+    );
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
