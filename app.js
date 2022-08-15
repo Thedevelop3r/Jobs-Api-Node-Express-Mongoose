0cd66820a7f0db248e858e7ae41df7184c77aa0d
Thedevelop3r <bilalarain444@gmai.com>
2022-08-16 01:24:16 +0500

    in-progress-refactor

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const connectDB = require("./db/connect");
+const connectDatabase = require("./db/connect");
 const app = express();
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -19,13 +20,19 @@ app.use("/api/v1/jobs", jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+})
+
 const port = process.env.PORT || 3000;
 
-const start = async () => {
+const start = () => {
   try {
-    await connectDB(process.env.MONGO_URI).then(
-      console.log("Mogoose Connection Successfull..")
-    );
+     connectDB(process.env.MONGO_URI);
+    
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
@@ -35,3 +42,14 @@ const start = async () => {
 };
 
 start();
+
+
+// // Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+    server.close(() => {
+        process.exit(1);
+    });
+});
--- controllers/auth.js
+++ controllers/auth.js
@@ -4,12 +4,7 @@ const { BadRequestError } = require("../errors");
 const bcryptjs = require("bcryptjs");
 
 const register = async (req, res) => {
-//   const { name, email, password } = req.body;
-//   if (!name || !email || !password) {
-//     throw new BadRequestError("name/email/password must be provided!");
-//   }
-
-  const user = await User.create({...req.body});
+  const user = await User.create({ ...req.body });
 
   res.status(StatusCodes.CREATED).json({
     user,
@@ -21,15 +16,3 @@ const login = async (req, res) => {
 };
 
 module.exports = { register, login };
-
-
-
-
-
-// const salt = await bcryptjs.genSalt(10);
-// const hashedPassword = await bcryptjs.hash(password, salt);
-//   const hashedPassword = await bcryptjs.hash(
-//     password,
-//     await bcryptjs.genSalt(10)
-//   );
-// const tempUser = { name, email, password: hashedPassword };
