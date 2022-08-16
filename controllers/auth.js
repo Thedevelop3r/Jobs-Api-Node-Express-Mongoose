c6f4bd60df082607733210442df75f46e64ce568
Bilal-Arain <bilalarain444@gmail.com>
2022-08-16 17:43:51 +0500

    in-progress

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,12 +2,17 @@ const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-
+  const token = jwt.sign(
+    { userId: user._id, name: user.name },
+    "JahazLudanPeyGya",
+    { expiresIn: "30d" }
+  );
   res.status(StatusCodes.CREATED).json({
-    user,
+    token,
   });
 };
 
