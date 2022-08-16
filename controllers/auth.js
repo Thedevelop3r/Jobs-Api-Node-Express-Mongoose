4e7eb45adcfd86ca7837b215bb0ec9a7fc6fa8d5
Bilal-Arain <bilalarain444@gmail.com>
2022-08-16 20:09:12 +0500

    validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+// register user with hashed password | create json-web-token for future athentication
+// sending token to browser for front-end auths-access
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
   const token = user.createJWT();
@@ -15,11 +17,39 @@ const register = async (req, res) => {
   });
 };
 
+// login user with authentication
 const login = async (req, res) => {
-  res.send("login User");
+  // data validation
+  const { email, password } = req.body;
+  if (!email || !password) {
+    // email of password emtpy ? then => sending BadRequest as a response
+    throw new BadRequestError("Please provide email-password!");
+  }
+  // user exists/not exist validation
+  const user = await User.findOne({ email });
+  // if user doesnt exist validation
+  if (!user) {
+    // user not found =>
+    // throw UnAthenticated error
+    throw new UnauthenticatedError("Invalid Credentials!");
+  }
+  // password validation  
+  const isPasswordCorrect = await user.comparePassword(password);
+  // if validation failed
+  if(!isPasswordCorrect){
+    throw new UnauthenticatedError("Invalid Credentials!");
+  }
+  
+  
+  // if user exists we create token and send it back to user with user data
+  const token = await user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    token,
+    user: user.name,
+  });
 };
 
-// 
+//
 
-module.exports = { register, login};
-
+module.exports = { register, login };
--- models/User.js
+++ models/User.js
@@ -24,22 +24,27 @@ const UserSchema = new mongoose.Schema({
     minlenght: 6,
   },
 });
-
+// password hashing
 UserSchema.pre("save", async function () {
   const salt = await bcryptjs.genSalt(10);
   this.password = await bcryptjs.hash(this.password, salt);
 });
-
+// getting the current user name
 UserSchema.methods.getName = function () {
   return this.name;
 };
-
+// generating json-web-token for current user and returning it back
 UserSchema.methods.createJWT = function () {
   return jwt.sign(
     { userId: this._id, name: this.name },
     process.env.JWTSECRATETOKEN,
     { expiresIn: process.env.JTW_LIFETIME }
   );
+};
+// comparing the userpassword
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcryptjs.compare(candidatePassword, this.password);
+  return isMatch;
 };
 
 module.exports = mongoose.model("User", UserSchema);
